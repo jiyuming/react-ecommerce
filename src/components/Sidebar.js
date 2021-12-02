06c9ec9c4bc5118011e06c4b056400feabccee07
jiyuming <ji.yuming@hotmail.com>
2021-12-02 03:54:34 -0500

    close function added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,14 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const isOpen = false;
+  const {isSidebarOpen, closeSidebar} = useProductsContext();
+
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
         <div className="sidebar-header">
           <img src={logo} className="logo" alt="logo" />
-          <button className="close-btn" type="button">
+          <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes></FaTimes>
           </button>
         </div>
@@ -23,7 +24,7 @@ const Sidebar = () => {
           {links.map(({ id, text, url }) => {
             return (
               <li key={id}>
-                <Link to={url}>{text}</Link>
+                <Link to={url} onClick={closeSidebar}>{text}</Link>
               </li>
             );
           })}
